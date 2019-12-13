3e1be946533eb0a60370b22afbec96ec49bc3e9c
paul <paul.18.sma@gmail.com>
2019-12-13 00:32:23 -0300

    adds users routes and controller

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,15 @@
 import express from "express";
+import indexRoutes from "./routes/users";
+
 
 const app = express();
+
+// middlewares
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+
+app.use(indexRoutes);
 
 app.listen(3000, () => {
     console.log('Server on port', 3000);
