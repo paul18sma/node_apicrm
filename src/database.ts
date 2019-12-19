415e4d196663746325c4c9fd47b4aad6542032e3
paul <paul.18.sma@gmail.com>
2019-12-19 01:08:23 -0300

    changes database connection to sequelize ORM

--- src/database.ts
+++
@@ -1,9 +0,0 @@
-import { Pool } from "pg";
-
-export const pool = new Pool({
-    user: 'node_user',
-    host: 'localhost',
-    password: 'node.uaa_1',
-    database: 'node_api_auth_db'
-})
-
---
+++ src/database/database.ts
@@ -0,0 +1,17 @@
+import { Sequelize } from 'sequelize';
+
+
+export const sequelize = new Sequelize(
+    'node_api_auth_db',
+    'node_user', 
+    'node.uaa_1', {
+    host: 'localhost',
+    dialect: 'postgres',
+  
+    pool: {
+      max: 5,
+      min: 0,
+      idle: 10000
+    },
+    logging: true
+  });
